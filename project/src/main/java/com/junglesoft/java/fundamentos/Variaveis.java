666f008226667186b2f4c8a74c5b0db4e45ff6a0
Alexandre Moreira <4lexandre.moreira@gmail.com>
2024-06-05 21:46:06 -0300

    feat: Criando variaveis

---
+++ project/src/main/java/com/junglesoft/java/fundamentos/Variaveis.java
@@ -0,0 +1,7 @@
+package com.junglesoft.java.fundamentos;
+public class Variaveis {
+    public static void main(String[] args) {
+        String nome = "Alexandre";
+        System.out.println("Olá, " + nome);
+    }
+}
